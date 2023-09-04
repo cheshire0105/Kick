763b36888fb77cba24cbe1ded061c388c6670c6a
Daisy Kim <doyeonyyy@gmail.com>
2023-09-04 20:02:16 +0900

    [김도연] feat: 수정하기 페이지에서 사진 추가 버튼 눌렀을시 ImagePicker 기능 구현 (Info.plist 에 Photo Library Usage 추가) (버그 있음)

--- KICK/Controller/MypageViewController.swift
+++ KICK/Controller/MypageViewController.swift
@@ -4,14 +4,19 @@
 //
 //  Created by cheshire on 2023/09/04.
 //
-
+import Foundation
 import UIKit
 
 class MypageViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addProfilePhoto: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupProfilePhoto()
     }
+    
     @IBAction func backToMypage(_ sender: Any) {
         if self.presentingViewController != nil {
              self.dismiss(animated: true)
@@ -19,5 +24,28 @@ class MypageViewController: UIViewController {
              self.navigationController?.popViewController(animated: true)
            }
     }
-    
+    func setupProfilePhoto() {
+        addProfilePhoto.addTarget(self, action: #selector(uploadPhoto), for: .touchUpInside)
+        view.addSubview(imageView)
+        view.addSubview(addProfilePhoto)
+    }
+    @objc func uploadPhoto() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
+}
+extension MypageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage =
+            info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                imageView.contentMode = .scaleAspectFit
+                imageView.image = pickedImage
+            }
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
