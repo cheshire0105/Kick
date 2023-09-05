aa4940a5cd8ad25bfd53b9f7719d53984502e49f
churoo <churoo0418@gmail.com>
2023-09-05 09:49:41 +0900

    [박철우] feat : lsign up button click --> event 회원가입 버튼을 눌렀을때 회원가입에대한 화면이 나옵니다. fixes : 여전히 버튼들의 간격이라던지 위치조정에 어려움음 겪고 있습니다.           스택뷰를 써야할거같습니다 그런대 어렵내요

--- KICK/Controller/Login/LoginViewController.swift
+++ KICK/Controller/Login/LoginViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import SnapKit
 
-
 class LoginViewController: UIViewController  {
     
     private let accountInputView = UIView()
@@ -35,11 +34,6 @@ class LoginViewController: UIViewController  {
     private let idTextField = {
         let textField = UITextField()
         textField.layer.cornerRadius = 10
-//        let attributes: [NSAttributedString.Key: Any] = [
-//            .foregroundColor: UIColor.gray,
-//            .font: UIFont.systemFont(ofSize: 5)
-//        ]
-//        textField.attributedPlaceholder = NSAttributedString(string: "아이디를 입력해보세요!", attributes: attributes)
         textField.placeholder = "아이디를 입력해보세요!"
         textField.textColor = UIColor.black
         textField.backgroundColor = UIColor.white
@@ -71,7 +65,7 @@ class LoginViewController: UIViewController  {
     
     private let logInButton = {
         let button = UIButton()
-        button.titleLabel?.text = "로그인"
+        button.setTitle("로그인", for: .normal)
         button.setTitleColor(UIColor.black, for: .normal)
         button.backgroundColor = UIColor.white
         return button
@@ -79,27 +73,18 @@ class LoginViewController: UIViewController  {
     
     private let signUpButton = {
         let button = UIButton()
-        button.titleLabel?.text = "회원가입"
+        button.setTitle("회원가입", for: .normal)
         button.setTitleColor(UIColor.black, for: .normal)
         button.backgroundColor = UIColor.white
         return button
     }()
     
     
-    @objc func logInButtonTapped() {
-        print("버튼")
-        let storyboard = UIStoryboard(name: "MainTabBar", bundle: nil)
-        if let tabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as? TabBarController {
-            tabBarController.modalPresentationStyle = .fullScreen
-            self.present(tabBarController, animated: true) {
-                self.navigationController?.viewControllers.remove(at: 0)
-            }
-        }
-    }
     
     
     
-/*--------------------------------------------------------*/
+    
+    /*--------------------------------------------------------*/
     
     
     
@@ -115,7 +100,7 @@ class LoginViewController: UIViewController  {
     
     
     
-/*--------------------------------------------------------*/
+    /*--------------------------------------------------------*/
     func configureAccountInputView () {
         
         for subview in [idLabel, idTextField, passwordLabel, passwordTextField, logInButton, signUpButton] {
@@ -127,19 +112,19 @@ class LoginViewController: UIViewController  {
             make.top.equalToSuperview()
             make.trailing.equalTo(passwordLabel)
         }
-        
+
         passwordLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.top.equalTo(idLabel.snp.bottom).offset(5)
             make.trailing.equalTo(idLabel)
         }
-        
+
         idTextField.snp.makeConstraints { make in
             make.leading.equalTo(idLabel.snp.trailing).offset(5)
             make.top.equalToSuperview()
             make.trailing.equalToSuperview()
         }
-        
+
         passwordTextField.snp.makeConstraints { make in
             make.leading.equalTo(passwordLabel.snp.trailing).offset(5)
             make.top.equalTo(idLabel.snp.bottom).offset(5)
@@ -147,11 +132,9 @@ class LoginViewController: UIViewController  {
         }
         
         logInButton.addTarget(self, action: #selector(logInButtonTapped), for: .touchUpInside)
+        
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
 
-//        logInButton.snp.makeConstraints { make in
-//            //make.top.equalTo(idLabel).offset(30)
-//            //make.bottom.equalTo(passwordLabel).offset(30)
-//        }
     }
     
     func configureLayout () {
@@ -170,7 +153,7 @@ class LoginViewController: UIViewController  {
         accountInputView.snp.makeConstraints { make in
             make.top.equalTo(logoImageView.snp.bottom).offset(50)
             make.leading.equalToSuperview().offset(20)
-           
+            
         }
         
         logInButton.snp.makeConstraints { make in
@@ -178,24 +161,32 @@ class LoginViewController: UIViewController  {
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
             make.top.equalTo(accountInputView)
             make.bottom.equalTo(signUpButton.snp.top).offset(-20)
-        
+            
         }
         
         signUpButton.snp.makeConstraints { make in
             make.top.equalTo(logInButton.snp.bottom).offset(20)
-            make.leading.equalToSuperview().offset(290)
+            make.leading.equalToSuperview().offset(260)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
         }
     }
     
-//    @IBAction func testButton(_ sender: UIButton) {
-//
-//        let storyboard = UIStoryboard(name: "MainTabBar", bundle: nil)
-//        if let tabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as? TabBarController {
-//            tabBarController.modalPresentationStyle = .fullScreen
-//            self.present(tabBarController, animated: true) {
-//                self.navigationController?.viewControllers.remove(at: 0)
-//            }
-//        }
-//    }
+    
+    @objc func logInButtonTapped() {
+        print("버튼")
+        let storyboard = UIStoryboard(name: "MainTabBar", bundle: nil)
+        if let tabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as? TabBarController {
+            tabBarController.modalPresentationStyle = .fullScreen
+            self.present(tabBarController, animated: true) {
+                self.navigationController?.viewControllers.remove(at: 0)
+            }
+        }
+    }
+    
+    @objc func signUpButtonTapped() {
+        let signUpViewController = SignUpViewController()
+        signUpViewController.modalPresentationStyle = .fullScreen
+        present(signUpViewController, animated: true, completion: nil)
+    }
+    
 }
---
+++ KICK/Controller/Login/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  KICK
+//
+//  Created by t2023-m0081 on 2023/09/05.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
