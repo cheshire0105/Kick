3c28bbadbf41bb6ea8dd04e6be1dd5a622905aaa
churoo <churoo0418@gmail.com>
2023-09-05 10:50:44 +0900

    [박철우] feat : app icon && launchScreen image add 앱아이콘 & 런치스크린 이미지 추가했습니다 -feat : 회원가입 페이지(SignUpViewController)view 작성중

--- KICK/Controller/Login/SignUpViewController.swift
+++ KICK/Controller/Login/SignUpViewController.swift
@@ -6,16 +6,115 @@
 //
 
 import UIKit
+import SnapKit
+import SwiftUI
+
+import SwiftUI
+
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+
+struct MyViewController_PreViews: PreviewProvider {
+    static var previews: some View {
+        SignUpViewController().toPreview()
+    }
+}
+
 
 class SignUpViewController: UIViewController {
 
+    private let makingAccountInputView = UIView()
+    
+    private let logoImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "")
+    }
+    
+    private let makingIdLabel = {
+        let label = UILabel()
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10
+        label.text = "New ID"
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let makingPasswordLabel = {
+        let label = UILabel()
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10
+        label.text = "new password"
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let checkingPasswordLabel = {
+        let label = UILabel()
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 10
+        label.text = "check password"
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let okButton = {
+        let button = UIButton()
+        button.setTitle("OK", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.white
+        return button
+    }()
+    
+    private let cancelButton = {
+        let button = UIButton()
+        button.setTitle("cancel", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.white
+        return button
+    }()
+    
+/*------------------------------------*/
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        configureMakingAccountInputView()
     }
     
+/*------------------------------------*/
 
+    func configureMakingAccountInputView () {
+
+        for subview in [ makingIdLabel, makingPasswordLabel, checkingPasswordLabel, okButton, cancelButton ] {
+            makingAccountInputView.addSubview(subview)
+        }
+    }
+
+    
+    
+    
 
 }
