0a2c826ba722d049399b4a148a797ec538be1749
churoo <churoo0418@gmail.com>
2023-09-05 14:17:40 +0900

    [박철우] test: 테스트

--- KICK/Controller/Login/LoginViewController.swift
+++ KICK/Controller/Login/LoginViewController.swift
@@ -91,8 +91,7 @@ class LoginViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGreen
-        
+        view.backgroundColor = .green
         configureAccountInputView()
         configureLayout()
     }
