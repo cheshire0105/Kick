fa58c0a83e38d68635f898fab632244d885f366e
Daisy Hong <hdaisywd@gmail.com>
2023-09-06 13:49:38 +0900

    [FEAT] Marker touchhandler added

--- KICK/Controller/MapViewController.swift
+++ KICK/Controller/MapViewController.swift
@@ -11,7 +11,7 @@ class MapViewController: UIViewController, NMFMapViewOptionDelegate  {
 
     private let registerButton = {
         let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "rectangle.fill"), for: .normal)
+        // button.setBackgroundImage(UIImage(systemName: "rectangle.fill"), for: .normal)
         button.addTarget(MapViewController.self, action: #selector(registerButtonAction), for: .touchUpInside)
         return button
     }()
@@ -35,21 +35,19 @@ class MapViewController: UIViewController, NMFMapViewOptionDelegate  {
                 // 메인 스레드
                 for marker in markers {
                     marker.mapView = self?.mapView
+                    marker.touchHandler = {(overlay: NMFOverlay) -> Bool in
+                        print("오버레이 터치됨")
+                        return true
+                    }
                 }
                 
                 print("marker 생성 완료")
                 print("map 생성 시작")
                 self?.configureMap()
                 self?.configureLayout()
-                
-
             }
 
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
     }
     
     func configureMap() {
@@ -74,12 +72,11 @@ class MapViewController: UIViewController, NMFMapViewOptionDelegate  {
         registerButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-10)
-            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(10)
-            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-10)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
         }
         positionButton.backgroundColor = .yellow
         positionButton.snp.makeConstraints { make in
-            //make.leading.equalTo(registerButton.snp.trailing).offset(-20)
             make.trailing.equalTo(registerButton.snp.trailing)
             make.bottom.equalTo(registerButton.snp.top).offset(-10)
         }
