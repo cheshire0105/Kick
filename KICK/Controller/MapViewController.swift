131a0008893ff7b74643758b9deaeab63d04f272
Daisy Hong <hdaisywd@gmail.com>
2023-09-06 12:10:18 +0900

    [FEAT] Marker added to map

--- KICK/Controller/MapViewController.swift
+++ KICK/Controller/MapViewController.swift
@@ -4,10 +4,11 @@ import SnapKit
 import NMapsMap
 
 class MapViewController: UIViewController, NMFMapViewOptionDelegate  {
-    
-    let button = UIButton() 
-    
+
     // Properties
+    private let mapView = NMFMapView()
+    private let naverMapView = NMFNaverMapView()
+
     private let registerButton = {
         let button = UIButton()
         button.setBackgroundImage(UIImage(systemName: "rectangle.fill"), for: .normal)
@@ -15,48 +16,79 @@ class MapViewController: UIViewController, NMFMapViewOptionDelegate  {
         return button
     }()
     
-    private let mapView = NMFMapView()
-    private let naverMapView = NMFNaverMapView()
-    
-    private let positionButton = {
-        let button = NMFLocationButton()
-        return button
-    }()
+    private let positionButton = NMFLocationButton()
+    private let zoomControlButton = NMFZoomControlView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureMap()
-        configureLayout()
+        DispatchQueue.global(qos: .default).async { [self] in
+            // 백그라운드 스레드
+            var markers = [NMFMarker]()
+            for index in 1...5 {
+                let marker = NMFMarker(position: NMGLatLng(lat: 37.36631851883025, lng: 127.10944555502921)
+, iconImage: NMFOverlayImage(name: "KickBoard"))
+                markers.append(marker)
+            }
+
+            DispatchQueue.main.async { [weak self] in
+                // 메인 스레드
+                for marker in markers {
+                    marker.mapView = self?.mapView
+                }
+                
+                print("marker 생성 완료")
+                print("map 생성 시작")
+                self?.configureMap()
+                self?.configureLayout()
+                
+
+            }
+
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
     func configureMap() {
-
+        /* 현재 위치 표시 */
+        mapView.positionMode = .normal
+        /* 현재 위치 찾아주는 버튼 설정 */
+        positionButton.mapView = naverMapView.mapView
+        /* 줌 버튼 */
+        zoomControlButton.mapView = naverMapView.mapView
     }
     
     func configureLayout() {
         view.addSubview(mapView)
         view.addSubview(registerButton)
-        
-        positionButton.mapView = naverMapView.mapView
         view.addSubview(positionButton)
-        
-        positionButton.snp.makeConstraints { make in
-            make.trailing.equalTo(registerButton.snp.trailing).offset(-10)
-            make.bottom.equalTo(registerButton.snp.top).offset(-10)
-            
-        }
+        view.addSubview(zoomControlButton)
         
         mapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        registerButton.snp.makeConstraints{ make in
+        registerButton.backgroundColor = .red
+        registerButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-10)
-            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-10)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(10)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-10)
         }
+        positionButton.backgroundColor = .yellow
+        positionButton.snp.makeConstraints { make in
+            //make.leading.equalTo(registerButton.snp.trailing).offset(-20)
+            make.trailing.equalTo(registerButton.snp.trailing)
+            make.bottom.equalTo(registerButton.snp.top).offset(-10)
+        }
+        zoomControlButton.backgroundColor = .blue
+        zoomControlButton.snp.makeConstraints { make in
+            make.leading.equalTo(positionButton.snp.leading)
+            make.bottom.equalTo(positionButton.snp.top)
+        }
+
     }
     
     @objc func registerButtonAction() {
